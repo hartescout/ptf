5de9d3c3dc823d4feb2615cc2e61bf570543a281
meatball <41243343+hartescout@users.noreply.github.com>
2020-05-08 16:42:24 -0700

    Bootstrap for Raspberry Pi4 armhf
    Bootstrap for Raspberry Pi4 armhf

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,15 +8,15 @@
 echo "docker-ptf" > /etc/hostname
 apt-get update
 apt-get upgrade -y
-apt-get install -y apt-utils wget locate python python3 python3-pip git sudo locate vim python-pip tcpdump net-tools flex
+apt-get install -y apt-utils wget locate python python3 python3-pip git sudo locate vim tcpdump net-tools flex
 apt --fix-broken install -y
 cd /tmp
-wget http://http.kali.org/pool/main/u/unicornscan/unicornscan_0.4.7-1kali2_amd64.deb
-dpkg -i ./unicornscan_0.4.7-1kali2_amd64.deb
+wget https://http.kali.org/pool/main/u/unicornscan/unicornscan-dbgsym_0.4.7-1kali2_armhf.deb
+dpkg -i ./unicornscan-dbgsym_0.4.7-1kali2_armhf.deb
 cd /root
 git clone https://github.com/trustedsec/ptf.git
 cd ptf
-echo -en "use modules/install_update_all\nyes\n" | python ptf
+echo -en "use modules/install_update_all\nyes\n" | python3 ptf
 echo
 echo
 echo "** DONE **"
